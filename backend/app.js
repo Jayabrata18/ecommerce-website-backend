be9471d216e2facd2f9914f738af479d68b8ea59
Jayabrata18 <jayabrata180402@gmail.com>
2023-02-23 19:46:38 +0530

    docker done

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
-require ("dotenv").config();
+require("dotenv").config();
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -14,12 +14,11 @@ connectDB();
 
 //define routes
 app.use(express.json({ extended: false }));
-app.use("/api/users", require("./routes/userApi"));
+app.use("/api/users", require("./routes/UserApi"));
 app.use("/api/products", require("./routes/productsApi"));
 app.use("/api/auth", require("./routes/authApi"));
 app.use("/api/profile", require("./routes/profileApi"));
 app.use("/api/cart", require("./routes/cartApi"));
-
 
 app.get("/", function (req, res) {
   res.send("Hello, world!");
